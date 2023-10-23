b6c3aaa62ef946589c59f55aaaa2acbdb2f82cdf
gustavo-fardo <78488285+gustavo-fardo@users.noreply.github.com>
2023-10-23 10:21:33 -0300

    Add files via upload

---
+++ yolov8_serial.py
@@ -0,0 +1,125 @@
+import torch
+import numpy as np
+import cv2
+from time import time
+from ultralytics import YOLO
+
+import supervision as sv
+import serial
+
+
+class ObjectDetection:
+
+    def __init__(self, capture_index, serial_port):
+       
+        self.capture_index = capture_index
+        
+        self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
+        print("Using Device: ", self.device)
+        
+        self.model = self.load_model()
+        
+        self.CLASS_NAMES_DICT = self.model.model.names
+    
+        self.box_annotator = sv.BoxAnnotator(sv.ColorPalette.default(), thickness=3, text_thickness=3, text_scale=1.5)
+        
+        self.serial_port = serial_port
+    
+
+    def load_model(self):
+       
+        model = YOLO("runs/detect/train3/weights/best.pt")  # load a pretrained YOLOv8n model
+        model.fuse()
+    
+        return model
+
+
+    def predict(self, frame):
+       
+        results = self.model(frame)
+        
+        return results
+    
+
+    def plot_bboxes(self, results, frame):
+        
+        xyxys = []
+        confidences = []
+        class_ids = []
+        
+         # Extract detections for person class
+        for result in results:
+            boxes = result.boxes.cpu().numpy()
+            class_id = boxes.cls[0]
+            # conf = boxes.conf[0]
+            # xyxy = boxes.xyxy[0]
+
+            if class_id == 0.0:
+              xyxy = result.boxes.xyxy.cpu().numpy()
+              conf = result.boxes.conf.cpu().numpy()
+              cls = result.boxes.cls.cpu().numpy().astype(int)
+              
+              byte_string = f"{xyxy} {conf} {cls}".encode('utf-8')
+              serial_port.write(byte_string)
+              
+              xyxys.append(xyxy)
+              confidences.append(conf)
+              class_ids.append(cls)
+            
+        
+        # Setup detections for visualization
+        detections = sv.Detections(
+                    xyxy=results[0].boxes.xyxy.cpu().numpy(),
+                    confidence=results[0].boxes.conf.cpu().numpy(),
+                    class_id=results[0].boxes.cls.cpu().numpy().astype(int),
+                    )
+        
+    
+        # Format custom labels
+        self.labels = [f"{self.CLASS_NAMES_DICT[class_id]} {confidence:0.2f}"
+        for _, _, confidence, class_id, tracker_id
+        in detections]
+        
+        # Annotate and display frame
+        frame = self.box_annotator.annotate(scene=frame, detections=detections, labels=self.labels)
+        
+        return frame
+    
+    
+    
+    def __call__(self):
+
+        cap = cv2.VideoCapture(self.capture_index)
+        assert cap.isOpened()
+        cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
+        cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
+      
+        while True:
+          
+            start_time = time()
+            
+            ret, frame = cap.read()
+            assert ret
+            
+            results = self.predict(frame)
+            frame = self.plot_bboxes(results, frame)
+            
+            end_time = time()
+            fps = 1/np.round(end_time - start_time, 2)
+             
+            cv2.putText(frame, f'FPS: {int(fps)}', (20,70), cv2.FONT_HERSHEY_SIMPLEX, 1.5, (0,255,0), 2)
+            
+            cv2.imshow('YOLOv8 Detection', frame)
+ 
+            if cv2.waitKey(5) & 0xFF == 27:
+                
+                break
+        
+        cap.release()
+        cv2.destroyAllWindows()
+        serial_port.close()
+        
+        
+serial_port = serial.Serial()
+detector = ObjectDetection(capture_index="inspecao_5.MP4", serial_port=serial_port)
+detector()
