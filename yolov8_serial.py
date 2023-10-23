481df0653325d0646605169c2e268cbb8c33cc21
gustavo-fardo <gustavo.fardo@hotmail.com>
2023-10-23 11:08:57 -0300

    adequate outputs

--- yolov8_serial.py
+++ yolov8_serial.py
@@ -10,25 +10,23 @@ import serial
 
 class ObjectDetection:
 
-    def __init__(self, capture_index, serial_port):
+    def __init__(self, capture_index, model_path, serial_port):
        
         self.capture_index = capture_index
         
         self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
         print("Using Device: ", self.device)
         
-        self.model = self.load_model()
+        self.model = self.load_model(model_path)
         
         self.CLASS_NAMES_DICT = self.model.model.names
-    
-        self.box_annotator = sv.BoxAnnotator(sv.ColorPalette.default(), thickness=3, text_thickness=3, text_scale=1.5)
         
         self.serial_port = serial_port
     
 
     def load_model(self):
        
-        model = YOLO("runs/detect/train3/weights/best.pt")  # load a pretrained YOLOv8n model
+        model = YOLO(model_path)  # load a pretrained YOLOv8n model
         model.fuse()
     
         return model
@@ -43,28 +41,26 @@ class ObjectDetection:
 
     def plot_bboxes(self, results, frame):
         
-        xyxys = []
-        confidences = []
-        class_ids = []
-        
-         # Extract detections for person class
+	byte_string = f"{time()}".encode('utf-8')
+    	serial_port.write(byte_string)
+        print(byte_string)
+
+        # Extract detections for person class
         for result in results:
             boxes = result.boxes.cpu().numpy()
             class_id = boxes.cls[0]
-            # conf = boxes.conf[0]
-            # xyxy = boxes.xyxy[0]
-
-            if class_id == 0.0:
-              xyxy = result.boxes.xyxy.cpu().numpy()
-              conf = result.boxes.conf.cpu().numpy()
-              cls = result.boxes.cls.cpu().numpy().astype(int)
-              
-              byte_string = f"{xyxy} {conf} {cls}".encode('utf-8')
-              serial_port.write(byte_string)
-              
-              xyxys.append(xyxy)
-              confidences.append(conf)
-              class_ids.append(cls)
+           
+	    xyxy = result.boxes.xyxy.cpu().numpy()
+	    conf = result.boxes.conf.cpu().numpy()
+	    cls = result.boxes.cls.cpu().numpy().astype(int)
+	     
+	    byte_string = f"{xyxy} {conf} {cls}".encode('utf-8')
+            print(byte_string)
+	    serial_port.write(byte_string)
+	    
+	    xyxys.append(xyxy)
+	    confidences.append(conf)
+	    class_ids.append(cls)
             
         
         # Setup detections for visualization
@@ -80,9 +76,6 @@ class ObjectDetection:
         for _, _, confidence, class_id, tracker_id
         in detections]
         
-        # Annotate and display frame
-        frame = self.box_annotator.annotate(scene=frame, detections=detections, labels=self.labels)
-        
         return frame
     
     
@@ -95,25 +88,12 @@ class ObjectDetection:
         cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
       
         while True:
-          
-            start_time = time()
             
             ret, frame = cap.read()
             assert ret
             
             results = self.predict(frame)
-            frame = self.plot_bboxes(results, frame)
-            
-            end_time = time()
-            fps = 1/np.round(end_time - start_time, 2)
-             
-            cv2.putText(frame, f'FPS: {int(fps)}', (20,70), cv2.FONT_HERSHEY_SIMPLEX, 1.5, (0,255,0), 2)
-            
-            cv2.imshow('YOLOv8 Detection', frame)
- 
-            if cv2.waitKey(5) & 0xFF == 27:
-                
-                break
+            frame = self.plot_bboxes(results, frame)      
         
         cap.release()
         cv2.destroyAllWindows()
@@ -121,5 +101,5 @@ class ObjectDetection:
         
         
 serial_port = serial.Serial()
-detector = ObjectDetection(capture_index="inspecao_5.MP4", serial_port=serial_port)
+detector = ObjectDetection(capture_index="inspecao_5.MP4", model="runs/detect/train3/weights/best.pt", serial_port=serial_port)
 detector()
